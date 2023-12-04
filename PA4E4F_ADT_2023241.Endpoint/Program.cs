e326d0833fe6d58538cd8dbfc5f1c5d74507f23b
nowan02 <starkgeri@gmail.com>
2023-12-04 15:39:49 +0100

    Added error handling to GET endpoints when read values are null

--- PA4E4F_ADT_2023241.Endpoint/Program.cs
+++ PA4E4F_ADT_2023241.Endpoint/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using PA4E4F_ADT_2023241.Endpoint;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -20,9 +21,33 @@ LogicFactory factory = new LogicFactory(
 app.MapGet("/", async context => await context.Response.WriteAsync("Working as intended!"));
 
 // Student GET
-app.MapGet("/Students", async context => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().ReadAll(), Formatting.Indented)));
-app.MapGet("/Students/{id}", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().Read(s => s.Id == id), Formatting.Indented)));
-app.MapGet("/Students/{id}/Grades", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().GetGradesOfStudent(id), Formatting.Indented)));
+app.MapGet("/Students", async context => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().ReadAll(), Formatting.Indented));
+    }
+    catch (Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Students/{id}", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().Read(s => s.Id == id), Formatting.Indented))
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Students/{id}/Grades", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().GetGradesOfStudent(id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
 app.MapGet("/Students/{id}/Subjects", async (HttpContext context, int id) => { try {
         await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().GetSubjectsOfStudent(id), Formatting.Indented));
     }
@@ -31,7 +56,15 @@ app.MapGet("/Students/{id}/Subjects", async (HttpContext context, int id) => { t
         await context.Response.WriteAsync(ex.Message);
     }
 });
-app.MapGet("/Students/{id}/Average", async (HttpContext context, int id) => await   context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().GetStudentAverage(id), Formatting.Indented)));
+app.MapGet("/Students/{id}/Average", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateStudentLogic().GetStudentAverage(id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
 
 // Student POST
 app.MapPost("/Students/{id}/Create", async (HttpContext context, int id) => await context.Response.WriteAsync(ObjectCreator.CreateObject<Student>(context, id, factory)));
@@ -45,10 +78,42 @@ app.MapPut("/Students/{id}/Drop/{SubId}", async (HttpContext context, int id, in
 app.MapDelete("/Students/{id}/Delete", async (HttpContext context, int id) => await context.Response.WriteAsync(ObjectDelete.Delete<Student>(context, id, factory)));
 
 // Teacher GET
-app.MapGet("/Teachers", async context => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().ReadAll(), Formatting.Indented)));
-app.MapGet("/Teachers/{id}", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().Read(t => t.Id == id), Formatting.Indented)));
-app.MapGet("/Teachers/{id}/Grades", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().GetGradesOfTeacher(id), Formatting.Indented)));
-app.MapGet("/Teachers/{id}/Subjects", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().GetTaughtSubjects(id), Formatting.Indented)));
+app.MapGet("/Teachers", async context => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().ReadAll(), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Teachers/{id}", async (HttpContext context, int id) => { try 
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().Read(t => t.Id == id), Formatting.Indented));
+    }
+    catch (Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Teachers/{id}/Grades", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().GetGradesOfTeacher(id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Teachers/{id}/Subjects", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateTeacherLogic().GetTaughtSubjects(id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
 
 // Teacher POST
 app.MapPost("/Teachers/{id}/Create", async (HttpContext context, int id) => await context.Response.WriteAsync(ObjectCreator.CreateObject<Teacher>(context, id, factory)));
@@ -61,11 +126,51 @@ app.MapPut("/Teachers/{id}/Update", async (HttpContext context, int id) => await
 app.MapDelete("/Teachers/{id}/Delete", async (HttpContext context, int id) => await context.Response.WriteAsync(ObjectDelete.Delete<Teacher>(context, id, factory)));
 
 // Subject GET
-app.MapGet("/Subjects", async context => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().ReadAll(), Formatting.Indented)));
-app.MapGet("/Subjects/{id}", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().Read(su => su.Id == id), Formatting.Indented)));
-app.MapGet("/Subjects/{id}/Students", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetStudentsOnSubject(id), Formatting.Indented)));
-app.MapGet("/Subjects/{id}/Teacher", async (HttpContext context, int id) => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetSubjectTeacher(id))));
-app.MapGet("/Subjects/{id}/Unattended", async context => await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetSubjectsWithNoTeacher())));
+app.MapGet("/Subjects", async context => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().ReadAll(), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Subjects/{id}", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().Read(su => su.Id == id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Subjects/{id}/Students", async (HttpContext context, int id) => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetStudentsOnSubject(id), Formatting.Indented));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
+app.MapGet("/Subjects/{id}/Teacher", async (HttpContext context, int id) => { try
+    { 
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetSubjectTeacher(id))); 
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }    
+});
+app.MapGet("/Subjects/{id}/Unattended", async context => { try
+    {
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(factory.CreateSubjectLogic().GetSubjectsWithNoTeacher()));
+    }
+    catch(Exception ex)
+    {
+        await context.Response.WriteAsync(ex.Message);
+    }
+});
 
 // Subject POST
 app.MapPost("/Subjects/{id}/Create", async (HttpContext context, int id) => await context.Response.WriteAsync(ObjectCreator.CreateObject<Subject>(context, id, factory)));
